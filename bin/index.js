49e4930d003e56af0776f5c5d675f2cf6d2ba16d
marckregio <marckregio@gmail.com>
2022-11-24 08:57:11 +0800

    task: build

--- bin/index.js
+++ bin/index.js
@@ -32,12 +32,45 @@ var require_logger = __commonJS({
   }
 });
 
+// src/utils/httpRequest.js
+var require_httpRequest = __commonJS({
+  "src/utils/httpRequest.js"(exports, module2) {
+    var axios = require("../node_modules/axios/dist/node/axios.cjs");
+    var secrets2 = require("../node_modules/config-dug/build/index.js").default;
+    var https = require("https");
+    var logger = require_logger();
+    var httpRequest = axios.create();
+    httpRequest.interceptors.request.use((axiosConfigParam) => {
+      const axiosConfig = axiosConfigParam;
+      axiosConfig.baseURL = secrets2.CONSUMER_WEBHOOK_URL;
+      axiosConfig.httpsAgent = new https.Agent({ rejectUnauthorized: false });
+      return axiosConfig;
+    });
+    httpRequest.interceptors.response.use(
+      (response) => response.data,
+      (error) => {
+        logger.error(error);
+        return Promise.reject(error);
+      }
+    );
+    module2.exports = httpRequest;
+  }
+});
+
 // src/consumer.js
 var require_consumer = __commonJS({
   "src/consumer.js"(exports, module2) {
-    var { Kafka, CompressionCodecs, CompressionTypes, logLevel } = require("../node_modules/kafkajs/index.js");
+    var {
+      Kafka,
+      CompressionCodecs,
+      CompressionTypes,
+      logLevel
+    } = require("../node_modules/kafkajs/index.js");
     var SnappyCodec = require("../node_modules/kafkajs-snappy/src/index.js");
     var logger = require_logger();
+    var _ = require("../node_modules/lodash/lodash.js");
+    var { v4: uuidv4 } = require("../node_modules/uuid/dist/index.js");
+    var httpRequest = require_httpRequest();
     var secrets2 = require("../node_modules/config-dug/build/index.js").default;
     CompressionCodecs[CompressionTypes.Snappy] = SnappyCodec;
     var Consumer2 = class {
@@ -45,22 +78,48 @@ var require_consumer = __commonJS({
         this.consumer = this.initConsumer();
       }
       initConsumer() {
-        logger.info("Initializing Consumer with clientID:" + secrets2.KAFKA_CLIENT_ID);
-        const hasSSL = secrets2.KAFKA_SSL === "true" ? true : false;
+        logger.info(
+          "Initializing Consumer with clientID:" + secrets2.KAFKA_CONSUMER_CLIENT
+        );
+        const hasSASL = secrets2.KAFKA_SASL === "true" ? true : false;
         const config = {
-          clientId: secrets2.KAFKA_CLIENT_ID,
-          brokers: secrets2.KAFKA_BROKERS,
+          clientId: secrets2.KAFKA_CONSUMER_CLIENT,
+          brokers: [secrets2.KAFKA_CONSUMER_BROKERS],
           ssl: secrets2.KAFKA_SSL,
           logLevel: logLevel.INFO
         };
-        if (hasSSL) {
+        if (hasSASL) {
           config.ssl = {
             mechanism: secrets2.KAFKA_SASL_MECHANISM,
             username: secrets2.KAFKA_SASL_USERNAME,
             password: secrets2.KAFKA_SASL_PASSWORD
           };
         }
-        return new Kafka(config);
+        const kafka = new Kafka(config);
+        const consumer = kafka.consumer({
+          groupId: secrets2.KAFKA_CONSUMER_CLIENT + "-" + uuidv4(),
+          heartbeatInterval: 2e3
+        });
+        return consumer;
+      }
+      async listen() {
+        const topic = secrets2.KAFKA_TOPIC_SUB;
+        await this.consumer.connect();
+        await this.consumer.subscribe({ topic, fromBeginning: false });
+        await this.consumer.run({
+          eachBatchAutoResolve: false,
+          eachBatch: this.triggerWebhook
+        });
+      }
+      async triggerWebhook({ batch, resolveOffset }) {
+        _.map(batch.messages, async (message) => {
+          const response = JSON.parse(message.value.toString());
+          logger.info(
+            `Received message with offset ${message.offset} from TM with cbi ${response.client_batch_id}`
+          );
+          await httpRequest.post(secrets2.CONSUMER_WEBHOOK_ENDPOINT, response);
+          resolveOffset(message.offset);
+        });
       }
     };
     module2.exports = Consumer2;
@@ -71,13 +130,14 @@ var require_consumer = __commonJS({
 var Consumer = require_consumer();
 var http = require("http");
 var secrets = require("../node_modules/config-dug/build/index.js").default;
-var initializeConsumer = () => {
+var initializeConsumer = async () => {
   const kafka = new Consumer();
+  await kafka.listen();
 };
 var port = secrets.CONSUMER_PORT;
 var server = http.createServer((req, res) => {
 });
 server.listen(port, () => {
-  console.log(`Server running at PORT:${port}`);
+  console.log(`ud-kafka-library is running at PORT:${port}`);
   initializeConsumer();
 });
